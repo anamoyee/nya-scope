85a5b13f76722c1324c96194a718c5b88efe7800
anamoyee <106310501+anamoyee@users.noreply.github.com>
2026-02-13 21:29:16 +0100

    Refine the implementation

--- src/nya_scope/_impl.py
+++ src/nya_scope/_impl.py
@@ -1,7 +1,33 @@
+from typing import Never
+
+
 class __NoInitMeta(type):
-	def __call__(cls, *_, **__):
-		raise RuntimeError("This class doesnt support initialization, it is a scope/container for other values or (static/class)methods only.")
+	def __call__(cls, *_, **__) -> Never:
+		"""### ⚠️ Calling this `nya_scope.Scope`-class (e.g. to try to create a new instance) will raise a TypeError.
+
+		- If you need to create an instance, remove the `Scope` base class from this class's definition
+		- If you need to access an attribute or a static/class method, remove the calling parenthesis `()`
+		"""
+
+		e = TypeError(
+			"This type doesnt support initialization; "  #
+			"it is a named scope for values or (static/class)methods only."
+		)
+
+		e.add_note(
+			"""
+
+Hint: try removing the call:
+
+  ExampleScope__().example_field
+                ^^
+           remove those
+"""[1:-1]
+		)
+
+		raise e
 
 
-class Scope(metaclass=__NoInitMeta):  # inherit metaclass
-	...
+class Scope(metaclass=__NoInitMeta):
+	def __new__(cls) -> Never:
+		raise TypeError(...)  # to appease the type checkers
